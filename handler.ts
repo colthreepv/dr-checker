62b7f0728060d17c0d45cc531e72aa016babf397
colthreepv <mister.gamer@gmail.com>
2018-10-18 20:06:01 +0200

    (style): standard style compliant

--- handler.ts
+++ handler.ts
@@ -1,48 +1,47 @@
-import { Handler, Context, Callback } from 'aws-lambda';
+import { Handler, Context, Callback } from 'aws-lambda'
 
 import { URL } from 'url'
 
 import * as request from 'request-promise-native'
 
 interface HelloResponse {
-  statusCode: number;
-  body: string;
+  statusCode: number
+  body: string
 }
 
 interface DockerToken {
-  token: string;
-  access_token: string;
-  expires_in: number;
+  token: string
+  access_token: string
+  expires_in: number
   /**
    * Example: 2018-10-18T17:22:55.893319601Z
    */
-  issued_at: string;
+  issued_at: string
 }
 
 interface DockerManifest {
-  schemaVersion: number;
+  schemaVersion: number
   /**
    * Example: 'library/node'
    */
-  name: string;
+  name: string
   /**
    * Example: '8'
    */
-  tag: string;
+  tag: string
   /**
    * Example: 'amd64'
    */
-  architecture: string;
+  architecture: string
   /**
    * Example: [{ blobSum: 'sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4' }]
    */
-  fsLayers: { blobSum: string }[];
+  fsLayers: { blobSum: string }[]
   /**
    * Not used for now
    */
   history: []
 }
-
 
 const URL_TOKENS = 'https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/node:pull'
 const PROJECT = 'library/node'
@@ -50,7 +49,6 @@ const TAG = '8'
 
 const TOKEN_URL = 'https://auth.docker.io/token'
 const REGISTRY_URL = 'https://registry-1.docker.io/v2/'
-
 
 function tokenGenerator (project: string = PROJECT) {
   const ACTION = 'Token Generation'
@@ -82,11 +80,10 @@ function retrieveManifest (tokenP: Promise<string>, project: string = PROJECT, t
   return manifestRequest
 }
 
-
 function manifestHandler (event: any, context: Context, callback: Callback) {
   const response: HelloResponse = {
     body: '',
-    statusCode: 200,
+    statusCode: 200
   }
 
   const manifestP = retrieveManifest(tokenGenerator())
@@ -99,16 +96,15 @@ function manifestHandler (event: any, context: Context, callback: Callback) {
     .then(() => callback(undefined, response))
 }
 
-
 const hello: Handler = (event: any, context: Context, callback: Callback) => {
   const response: HelloResponse = {
     statusCode: 200,
     body: JSON.stringify({
       message: Math.floor(Math.random() * 10)
     })
-  };
+  }
 
-  callback(undefined, response);
-};
+  callback(undefined, response)
+}
 
 export { hello, manifestHandler }
