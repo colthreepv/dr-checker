4e325a6a9f536015aa639c5baf231a427354f02d
colthreepv <mister.gamer@gmail.com>
2018-10-28 02:29:30 +0100

    semaphores, tokenbanks & more code, less fixme

--- docker-registry.ts
+++ docker-registry.ts
@@ -1,9 +1,12 @@
+import promiseLimit from 'promise-limit'
 import * as request from 'request-promise-native'
-import { config } from './config'
+import { Config } from './config'
 import { Status } from './update-status'
 
 const TOKEN_URL = 'https://auth.docker.io/token'
 const REGISTRY_URL = 'https://registry-1.docker.io/v2/'
+
+const MAX_CONCURRENT_MANIFEST_REQUESTS = 2
 
 interface DockerToken {
   token: string
@@ -39,13 +42,32 @@ interface DockerManifest {
   history: any[]
 }
 
+const tokenBank: { [key: string]: { token: string, expiry: Date } } = {}
+
 export function tokenGenerator (project: string) {
+  // fast return in case token it's in bank
+  if (tokenBank[project] != null) {
+    const nowDate = new Date()
+    if (nowDate < tokenBank[project].expiry) return Promise.resolve(tokenBank[project].token)
+  }
+
   const ACTION = 'Token Generation'
   const queryParams = {
     service: 'registry.docker.io',
     scope: `repository:${ project }:pull`
   }
   const dockerRequest = request.get(TOKEN_URL, { qs: queryParams, json: true })
+  // banks the token
+  dockerRequest.then((data: DockerToken) => {
+    const expiryDate = new Date()
+    expiryDate.setUTCSeconds(expiryDate.getUTCSeconds() + data.expires_in)
+
+    tokenBank[project] = {
+      token: data.token,
+      expiry: expiryDate
+    }
+  })
+
   const tokenPromise = dockerRequest.then((data: DockerToken) => data.token)
 
   dockerRequest.catch((err) => {
@@ -56,7 +78,9 @@ export function tokenGenerator (project: string) {
 }
 
 // retrieves manifest from the docker image, with all the layers hashes
-export function retrieveManifest (tokenP: Promise<string>, project: string, tag: string): Promise<DockerManifest> {
+export function retrieveManifest (token: string | Promise<string>, project: string, tag: string): Promise<DockerManifest> {
+  const tokenP = Promise.resolve(token)
+
   const ACTION = 'Manifest Retrieval'
   const manifestURL = new URL(`${ project }/manifests/${ tag }`, REGISTRY_URL)
   const manifestRequest = tokenP.then((token) => {
@@ -73,8 +97,35 @@ export function retrieveManifest (tokenP: Promise<string>, project: string, tag:
 // compares new image layer hashes to the saved ones, returns a Status
 function checkImageHashChange (lastHash: string, currentHash: string): Status {}
 
+const manifestLimit = promiseLimit(MAX_CONCURRENT_MANIFEST_REQUESTS)
+
 // loops over config.image and checks manifests and compares for each one
 // ultimately producing a Status
-function checkAllImages (config: Config) {}
+export function checkAllImages (config: Config) {
+  const manifestPromises = []
+  for (let index = 0; index < config.image.length; index++) {
+    const project = config.image[index]
 
+    const projectTokenP = tokenGenerator(project.repository)
 
+    for (let projectIndex = 0; projectIndex < project.tags.length; projectIndex++) {
+      const tag = project.tags[projectIndex]
+
+      // adds a promise to the bunch
+      manifestPromises.push(
+        projectTokenP.then(token => { // assure there is a token
+          return manifestLimit(() => { // await the semaphore is green
+            return retrieveManifest(token, project.repository, tag)
+          })
+        })
+      )
+
+    }
+  }
+
+  const newStatus: Status = {}
+
+  // FIXME: create a new status based on the manifests retrieved
+  Promise.all(manifestPromises).then(manifests => {})
+
+}
